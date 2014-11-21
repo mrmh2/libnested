c81f38f5fe731d2ce48c688f7eb534d33478e949
Matthew Hartley <mhartley@cantab.net>
2014-11-21 14:31:16 +0000

    Added Catch for unit testing, split binaries

--- models.h
+++ models.h
@@ -5,3 +5,4 @@
 using namespace std;
 
 vector<double> logisticModel(const vector<double>& t, const vector<double>& params);
+vector<double> linearModel(const vector<double>& t, const vector<double>& params);
